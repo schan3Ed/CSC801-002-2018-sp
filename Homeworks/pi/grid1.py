c990ecc9d74a9da3aff035f97738530c72fb1a47
Edward Chan <schan3@ncsu.edu>
2018-02-02 16:24:43 -0500

    added main

--- Homeworks/pi/grid1.py
+++ Homeworks/pi/grid1.py
@@ -1,6 +1,8 @@
 
 piVal = 3.14
-def __main__(lineWidth=10, needleLength=20, probLmt = 100, 
+
+def singleExperiment(self):
+    return
+def __main__(self, lineWidth=10, needleLength=20, probLmt=100, 
     errInterval=0.005):
-    
     return
