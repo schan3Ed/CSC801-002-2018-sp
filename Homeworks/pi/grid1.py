a52289095fa2632bd9023ffbf71ecf1a93db805f
Edward Chan <schan3@ncsu.edu>
2018-02-02 16:32:44 -0500

    added basic structure

--- Homeworks/pi/grid1.py
+++ Homeworks/pi/grid1.py
@@ -1,8 +1,20 @@
 
-piVal = 3.14
+BKV = 3.14
 
-def singleExperiment(self):
-    return
-def __main__(self, lineWidth=10, needleLength=20, probLmt=100, 
-    errInterval=0.005):
-    return
+class grid1():
+    def singleDrop(self, lw, nl):
+        "dropping a single needle"
+        return False
+        
+    def singleExperiment(self, lw, nl, pl, err):
+        "running a single experiment"
+        return
+
+    def __main__(self, lineWidth=10, needleLength=20, probLmt=100, 
+        errInterval=0.05, experimentCnt=50):
+        "main method for parallel line"
+
+        for i in range(experimentCnt):
+            singleExperiment(lineWidth, needleLength, probLmt, errInterval)
+        return
+
