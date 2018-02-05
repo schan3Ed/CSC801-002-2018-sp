7966402684e318313b0cd8e0ed8ec2193c7f31c8
Edward Chan <schan3@ncsu.edu>
2018-02-04 20:39:05 -0500

    missing single Drop and output messages

--- Homeworks/pi/grid1.py
+++ Homeworks/pi/grid1.py
@@ -1,20 +1,36 @@
-
+import random
 BKV = 3.14
+def singleDrop():
+    "dropping a single needle"
+    return False
+    
+def singleExperiment(pl, err, seed):
+    "running a single experiment"
+    cnt = 0
+    hit = 0
+    pi = 0
+    BKV_upper = err + BKV
+    BKV_lower = BKV - err
+    for i in range(pl):
+        if(singleDrop()):
+            hit += 1
+        cnt += 1
+        pi = 2 * hit / cnt
+        if pi > BKV_lower and pi < BKV_upper:
+            return pi, cnt, False
+    return -1, cnt, True
 
-class grid1():
-    def singleDrop(self, lw, nl):
-        "dropping a single needle"
-        return False
-        
-    def singleExperiment(self, lw, nl, pl, err):
-        "running a single experiment"
-        return
+def run(probLmt=100, errInterval=0.05, experimentCnt=50, seed=None):
+    "main method for parallel line"
+    entry = []
+    seed = seed or random.random() * 9999
+    for i in range(experimentCnt):
+        isCensored = False
+        pi, cnt, isCensored = singleExperiment(probLmt, errInterval, seed)
+        entry.append([pi, cnt, isCensored])
+    print(entry)
+    return
 
-    def __main__(self, lineWidth=10, needleLength=20, probLmt=100, 
-        errInterval=0.05, experimentCnt=50):
-        "main method for parallel line"
-
-        for i in range(experimentCnt):
-            singleExperiment(lineWidth, needleLength, probLmt, errInterval)
-        return
+if __name__ == "__main__":
+    run()
 
